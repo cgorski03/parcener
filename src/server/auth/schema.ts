674c8ccac4eeaa57016d8238a79e28dfa53a84d5
Colin Gorski <cgorski03@gmail.com>
2025-12-28 10:31:27 -0500

    fix: session settings

--- src/server/auth/schema.ts
+++ src/server/auth/schema.ts
@@ -18,6 +18,21 @@ export const authConfig = {
                 input: false,
             },
         },
-    }
+    },
+    // We want sessions to persist. This is not an application users are constantly on, but they should 
+    // be able to click on someones room and still be logged in, even a week later.
+    // Its also just really not security-critical
+    session: {
+        // Set expiration to 90 days (in seconds)
+        expiresIn: 60 * 60 * 24 * 90,
+        // Update the session expiry if they visit and the 
+        // session is older than 1 day
+        updateAge: 60 * 60 * 24,
+        // Ensure the cookie is persistent 
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 60 * 24 * 90,
+        },
+    },
 } satisfies BetterAuthOptions;
 
