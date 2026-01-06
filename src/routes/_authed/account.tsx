e067d79edb2264a57ac1a269a00f54b8e9f3f0b1
Colin Gorski <cgorski03@gmail.com>
2026-01-05 21:10:07 -0500

    refactor: initial vertical slice plan

--- src/routes/_authed/account.tsx
+++ src/routes/_authed/account.tsx
@@ -1,9 +1,10 @@
+import { AccountRouteComponent } from '@/features/account/routes'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authed/account')({
-  component: RouteComponent,
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/account"!</div>
+    return <AccountRouteComponent />
 }
